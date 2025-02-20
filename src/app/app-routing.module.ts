9fba8484878022a681338c967531e4ac33b87bcf
Lyden CHAI <lyden.chai@gmail.com>
2025-02-20 10:07:04 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,74 +7,43 @@ const routes: Routes = [
     path: 'home',
     loadComponent: () =>
       import('./pages/home/components/home-page/home.page').then(
-        (m) => m.HomePage
+        (p) => p.HomePage
       ),
   },
   {
     path: 'category',
     loadComponent: () =>
       import('./pages/category/components/category-page/category.page').then(
-        (m) => m.CategoryPage
+        (p) => p.CategoryPage
       ),
   },
   {
     path: 'favorites',
     loadComponent: () =>
       import('./pages/favorites/components/favorites-page/favorites.page').then(
-        (m) => m.FavoritesPage
+        (p) => p.FavoritesPage
       ),
   },
   {
     path: 'archived',
     loadComponent: () =>
       import('./pages/archived/components/acrhived-page/archived.page').then(
-        (m) => m.ArchivedPage
+        (p) => p.ArchivedPage
       ),
   },
   {
     path: 'trash',
     loadComponent: () =>
       import('./pages/trash/components/trash-page/trash.page').then(
-        (m) => m.TrashPage
+        (p) => p.TrashPage
       ),
   },
   {
     path: 'spam',
     loadComponent: () =>
       import('./pages/spam/components/spam-page/spam.page').then(
-        (m) => m.SpamPage
+        (p) => p.SpamPage
       ),
-  },
-  {
-    path: 'category',
-    loadChildren: () =>
-      import('./pages/category/category.module').then(
-        (m) => m.CategoryPageModule
-      ),
-  },
-  {
-    path: 'favorites',
-    loadChildren: () =>
-      import('./pages/favorites/favorites.module').then(
-        (m) => m.FavoritesPageModule
-      ),
-  },
-  {
-    path: 'archived',
-    loadChildren: () =>
-      import('./pages/archived/archived.module').then(
-        (m) => m.ArchivedPageModule
-      ),
-  },
-  {
-    path: 'trash',
-    loadChildren: () =>
-      import('./pages/trash/trash.module').then((m) => m.TrashPageModule),
-  },
-  {
-    path: 'spam',
-    loadChildren: () =>
-      import('./pages/spam/spam.module').then((m) => m.SpamPageModule),
   },
 ];
 
